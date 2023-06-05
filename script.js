11f24ddcfde3d21fad01e5ebd261e3443aa074c5
AjmalKBangash <ak1489007@gmail.com>
2023-06-05 23:56:53 +0430

    first and last commit for this kiddy project

---
+++ script.js
@@ -0,0 +1,24 @@
+// This code has benn already transformed to .html file but incase you need separation of concerns then here it is,
+// function add(){
+//     let x = document.getElementById("firsti").value;
+//     let y = document.getElementById("secondi").value;
+//     document.getElementById("result").value = Number(x) + Number(y) ;
+// }
+
+// function sub(){
+//     let x = document.getElementById("firsti").value;
+//     let y = document.getElementById("secondi").value;
+//     document.getElementById("result").value = Number(x) - Number(y) ;
+// }
+
+// function mul(){
+//     let x = document.getElementById("firsti").value;
+//     let y = document.getElementById("secondi").value;
+//     document.getElementById("result").value = Number(x) * Number(y) ;
+// }
+
+// function div(){
+//     let x = document.getElementById("firsti").value;
+//     let y = document.getElementById("secondi").value;
+//     document.getElementById("result").value = Number(x) / Number(y) ;
+// }
